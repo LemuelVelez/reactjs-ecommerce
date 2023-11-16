f0de26c64dadaef11d5239aae23d386f10227c67
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-16 10:55:16 +0800

    update app.js with axios.interceptors and update navbar

--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -1,7 +1,45 @@
+import axios from "axios";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import swal from "sweetalert";
 
 function Navbar() {
+
+    const history = useHistory();
+    const logoutSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res => {
+            if (res.data.status === 200) {
+                localStorage.removeItem('auth_token');
+                localStorage.removeItem('auth_name');
+                swal("Success", res.data.message, "success");
+                history.push('/');
+            }
+        });
+    }
+
+    var AuthButtons = '';
+    if (!localStorage.getItem('auth_token')) {
+        AuthButtons = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Register</Link>
+                </li>
+            </ul>
+        );
+    }
+    else {
+        AuthButtons = (
+            <li className="nav-item">
+                <button type="button" onClick={logoutSubmit} className="nav-link btn btn-danger btn-sm text-white">Logout</button>
+            </li>
+        );
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-primary shadow sticky-top">
             <div className="container-fluid">
@@ -17,12 +55,7 @@ function Navbar() {
                         <li className="nav-item">
                             <Link className="nav-link" to="#">Collection</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
+                        {AuthButtons}
                     </ul>
                 </div>
             </div>
