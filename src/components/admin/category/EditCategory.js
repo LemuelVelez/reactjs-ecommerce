64157333a13ccbcb17ef7eb5656083e057b4c8f0
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-21 01:15:16 +0800

    add editcategory.js, update category, routes

--- src/components/admin/category/EditCategory.js
+++ src/components/admin/category/EditCategory.js
@@ -1,76 +1,41 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom";
 import swal from "sweetalert";
 
-function Category() {
-    const [categoryInput, setCategory] = useState({
-        slug: "",
-        name: "",
-        descrip: "",
-        status: "",
-        meta_title: "",
-        meta_keyword: "",
-        meta_descrip: "",
-        error_list: [],
-    });
+function EditCategory(props) {
+
+    const history = useHistory();
+    const [categoryInput, setCategory] = useState([]);
+
+    useEffect(() => {
+        const category_id = props.match.params.id;
+        axios.get(`api/edit-category/${category_id}`).then(res => {
+            if (res.data.status === 200) {
+                setCategory(res.data.category);
+            }
+            else if (res.data.status === 404) {
+                swal("Error", res.data.message, "error");
+                history.push(`/admin/view-category`);
+            }
+        });
+    }, [props.match.params.id, history]);
 
     const handleInput = (e) => {
         e.persist();
         setCategory({ ...categoryInput, [e.target.name]: e.target.value });
-    };
-
-    const submitCategory = async (e) => {
-        e.preventDefault();
-
-        const data = {
-            slug: categoryInput.slug,
-            name: categoryInput.name,
-            description: categoryInput.descrip,
-            status: categoryInput.status,
-            meta_title: categoryInput.meta_title,
-            meta_descrip: categoryInput.meta_descrip,
-        };
-
-        try {
-            const res = await axios.post(`/api/store-category`, data);
-
-            if (res.data.status === 200) {
-                swal("Success", res.data.message, "success");
-                document.getElementById("CATEGORY_FORM").reset();
-            } else if (res.data.status === 400) {
-                setCategory({ ...categoryInput, error_list: res.data.errors });
-            }
-        } catch (error) {
-            console.error("An error occurred:", error);
-            // Handle the error appropriately
-        }
-    };
-
-    var display_errors = [];
-    if (categoryInput.error_list) {
-        display_errors = [
-            categoryInput.error_list.slug,
-            categoryInput.error_list.name,
-            categoryInput.error_list.meta_title,
-        ];
     }
 
     return (
-        <div className="container-fluid px-4">
-
-            {display_errors.map((item, index) => {
-                return <p className="mb-1" key={index}>{item}</p>;
-            })}
-
+        <div className="container px-4">
             <div className="card mt-4">
                 <div className="card-header">
-                    <h4>Add Category
-                        <Link to="/admin/view-category" className="btn btn-primary btn-sm float-end">View Category</Link>
+                    <h4>Edit Category
+                        <Link to="/admin/view-category" className="btn btn-primary btn-sm float-end">BACK</Link>
                     </h4>
                 </div>
                 <div className="card-body">
-                    <form onSubmit={submitCategory} id="CATEGORY_FORM">
+                    <form>
                         <ul className="nav nav-tabs" id="myTab" role="tablist">
                             <li className="nav-item" role="presentation">
                                 <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
@@ -91,7 +56,7 @@ function Category() {
                                 </div>
                                 <div className="form-group mb-3">
                                     <label>Description</label>
-                                    <textarea name="descrip" onChange={handleInput} value={categoryInput.descrip} className="form-control"></textarea>
+                                    <textarea name="description" onChange={handleInput} value={categoryInput.description} className="form-control"></textarea>
                                 </div>
                                 <div className="form-group mb-3">
                                     <label>Status</label>
@@ -109,16 +74,16 @@ function Category() {
                                 </div>
                                 <div className="form-group mb-3">
                                     <label>Meta Description</label>
-                                    <textarea name="meta_descrip" onChange={handleInput} value={categoryInput.meta_descrip} className="form-control"></textarea>
+                                    <textarea name="meta_description" onChange={handleInput} value={categoryInput.meta_description} className="form-control"></textarea>
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" className="btn btn-primary px-4 float-end">Submit</button>
+                        <button type="submit" className="btn btn-primary px-4 float-end">Update</button>
                     </form>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default Category;
+export default EditCategory;
