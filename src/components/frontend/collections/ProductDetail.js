399f9260f21f67cf668032a7aa1533ea73c9290f
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-25 22:53:48 +0800

    add productdetail, update viewcategory, viewproduct, publicroutelist

--- src/components/frontend/collections/ProductDetail.js
+++ src/components/frontend/collections/ProductDetail.js
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import swal from 'sweetalert';
 import { useHistory } from 'react-router-dom';
 
-function ProductDetail(props)
-{
+function ProductDetail(props) {
 
     const history = useHistory();
     const [loading, setLoading] = useState(true);
@@ -17,18 +16,15 @@ function ProductDetail(props)
 
         const category_slug = props.match.params.category;
         const product_slug = props.match.params.product;
-        axios.get(`/api/viewproductdetail/${category_slug}/${product_slug}`).then(res=>{
-            if(isMounted)
-            {
-                if(res.data.status === 200)
-                {
+        axios.get(`/api/viewproductdetail/${category_slug}/${product_slug}`).then(res => {
+            if (isMounted) {
+                if (res.data.status === 200) {
                     setProduct(res.data.product);
                     setLoading(false);
                 }
-                else if(res.data.status === 404)
-                {
+                else if (res.data.status === 404) {
                     history.push('/collections');
-                    swal("Warning",res.data.message,"error");
+                    swal("Warning", res.data.message, "error");
                 }
             }
         });
@@ -40,12 +36,12 @@ function ProductDetail(props)
 
     // Quantity Increment/Decrement in Hooks - Start
     const handleDecrement = () => {
-        if(quantity > 1){
+        if (quantity > 1) {
             setQuantity(prevCount => prevCount - 1);
         }
     }
     const handleIncrement = () => {
-        if(quantity < 10){
+        if (quantity < 10) {
             setQuantity(prevCount => prevCount + 1);
         }
     }
@@ -53,40 +49,37 @@ function ProductDetail(props)
 
     const submitAddtocart = (e) => {
         e.preventDefault();
-        
+
         const data = {
             product_id: product.id,
             product_qty: quantity,
         }
 
-        axios.post(`/api/add-to-cart`, data).then(res=>{
-            if(res.data.status === 201){
+        axios.post(`/api/add-to-cart`, data).then(res => {
+            if (res.data.status === 201) {
                 //Created - Data Inserted
-                swal("Success",res.data.message,"success");
-            }else if(res.data.status === 409){
+                swal("Success", res.data.message, "success");
+            } else if (res.data.status === 409) {
                 //Already added to cart
-                swal("Success",res.data.message,"success");
-            }else if(res.data.status === 401){
+                swal("Success", res.data.message, "success");
+            } else if (res.data.status === 401) {
                 //Unauthenticated
-                swal("Error",res.data.message,"error");
-            }else if(res.data.status === 404){
+                swal("Error", res.data.message, "error");
+            } else if (res.data.status === 404) {
                 //Not Found
-                swal("Warning",res.data.message,"warning");
+                swal("Warning", res.data.message, "warning");
             }
         });
 
     }
 
-    if(loading)
-    {
+    if (loading) {
         return <h4>Loading Product Detail...</h4>
     }
-    else
-    {
+    else {
 
         var avail_stock = '';
-        if(product.qty > 0)
-        {
+        if (product.qty > 0) {
             avail_stock = <div>
                 <label className="btn-sm btn-success px-4 mt-2">In stock</label>
                 <div className="row">
@@ -103,8 +96,7 @@ function ProductDetail(props)
                 </div>
             </div>
         }
-        else
-        {
+        else {
             avail_stock = <div>
                 <label className="btn-sm btn-danger px-4 mt-2">Out of stock</label>
             </div>
@@ -133,17 +125,17 @@ function ProductDetail(props)
                                 <span className="float-end badge btn-sm btn-danger badge-pil"> {product.brand} </span>
                             </h4>
                             <p> {product.description} </p>
-                            <h4 className="mb-1"> 
-                                Rs: {product.selling_price}
-                                <s className="ms-2">  Rs: {product.original_price} </s>
+                            <h4 className="mb-1">
+                                ₱ {product.selling_price}
+                                <s className="ms-2">  ₱ {product.original_price} </s>
                             </h4>
                             <div>
                                 {avail_stock}
                             </div>
 
                             <button type="button" className="btn btn-danger mt-3">Add to Wishlist</button>
-                           
-                       </div>
+
+                        </div>
 
                     </div>
                 </div>
