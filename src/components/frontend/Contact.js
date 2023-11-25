daedca950e6cd27279aace447568656c2b9f0381
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2023-11-25 18:33:44 +0800

    add axios on view category, other update codes

--- src/components/frontend/Contact.js
+++ src/components/frontend/Contact.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Navbar from '../../layouts/frontend/Navbar';
+
 
 function Contact() {
     return (
         <div>
-            <Navbar />
             <h1>I am Contact Page</h1>
         </div>
     )
--- src/routes/Publicroutelist.js
+++ src/routes/Publicroutelist.js
@@ -6,11 +6,11 @@ import Page404 from '../components/errors/Page404';
 import Register from '../components/frontend/auth/Register';
 import Login from '../components/frontend/auth/Login';
 import ViewCategory from '../components/frontend/collections/ViewCategory';
-import ViewProduct from '../components/frontend/collections/ViewProduct';
-import ProductDetail from '../components/frontend/collections/ProductDetail';
-import Cart from '../components/frontend/Cart';
-import Checkout from '../components/frontend/Checkout';
-import Thankyou from '../components/frontend/Thankyou';
+// import ViewProduct from '../components/frontend/collections/ViewProduct';
+// import ProductDetail from '../components/frontend/collections/ProductDetail';
+// import Cart from '../components/frontend/Cart';
+// import Checkout from '../components/frontend/Checkout';
+// import Thankyou from '../components/frontend/Thankyou';
 
 const publicRoutesList = [
     { path: '/', exact: true, name: 'Home', component: Home },
@@ -21,11 +21,11 @@ const publicRoutesList = [
     { path: '/login', exact: true, name: 'Login', component: Login },
     { path: '/register', exact: true, name: 'Register', component: Register },
     { path: '/collections', exact: true, name: 'ViewCategory', component: ViewCategory },
-    { path: '/collections/:slug', exact: true, name: 'ViewProduct', component: ViewProduct },
-    { path: '/collections/:category/:product', exact: true, name: 'ProductDetail', component: ProductDetail },
-    { path: '/cart', exact: true, name: 'Cart', component: Cart },
-    { path: '/checkout', exact: true, name: 'Checkout', component: Checkout },
-    { path: '/thank-you', exact: true, name: 'Thankyou', component: Thankyou },
+    // { path: '/collections/:slug', exact: true, name: 'ViewProduct', component: ViewProduct },
+    // { path: '/collections/:category/:product', exact: true, name: 'ProductDetail', component: ProductDetail },
+    // { path: '/cart', exact: true, name: 'Cart', component: Cart },
+    // { path: '/checkout', exact: true, name: 'Checkout', component: Checkout },
+    // { path: '/thank-you', exact: true, name: 'Thankyou', component: Thankyou },
 ];
 
 export default publicRoutesList;
